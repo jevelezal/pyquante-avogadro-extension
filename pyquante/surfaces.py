2b38d11ad3a8360d197ac953ba66c0770ba3233c
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-05-26 18:10:23 +0200

    decided how to generate the mesh etc, need testing / refinements

--- pyquante/surfaces.py
+++ pyquante/surfaces.py
@@ -14,8 +14,14 @@
 
 '''
 import logging
+from PyQt4.QtCore import QSettings
+
 #import Avogadro
+import numpy
+from numpy import array
 from mock import Mock
+
+QSettings = Mock()
 Avogadro=Mock()
 
 class GlViewWrapper(object):
@@ -33,27 +39,56 @@ class GlViewWrapper(object):
             logging.debug("Name %s, Alias %s, PrimitiveTypes %s"%(
                     engine.name, engine.alias, engine.primitiveTypes))
         return surfaceEngine
+
+    def get_baricenter(self):
+        '''
+        get the approximate baricenter of the molecule
+        '''
+        ret = numpy.zeros(3)
+        for atom in self.glwidget.molecule.atoms:
+            ret += numpy.array(atom.pos) * atom.atomicNumber * 2
+        return ret
+
     def create_cube(self, dim,max,min):
         cube = self.glwidget.molecule.addCube()
         
         cube.setLimits(min,max,dim)
         return cube
+    
     def generate_mesh(self, cube):
         mesh = self.glwidget.molecule.addMesh()
         mesh.cube = cube.id
         return mesh
-    
-    def isosurface(self, voldata, iso):
+        
+    def isosurface(self, func, iso,
+                   dim = array([50,50,50]),
+                   min = -array([10.0,10.0,10.0]),
+                   max = array([10.0,10.0,10.0])):
         '''
-        Generate an isosurface
+        Generate an isosurface from the func (x,y,z) passed.
         '''
         # Create Cube (maybe extimate the size)
-        cube = self.create_cube()
+        center = self.get_baricenter() # Translating to the center of the molecule
+        min+=center
+        max+=center
+        cube = self.create_cube(dim, min, max)
+
         # Fill Data in the cube
+        x_min,y_min,z_min = min 
+        x_max,y_max,z_max = max
+        x_dim, y_dim, z_dim = dim
+        
+        # Creating a grid
+        grid = numpy.ogrid[x_min:x_max:x_dim*1j,
+                           y_min:y_max:z_dim*1j,
+                           z_min:z_max:z_dim*1j]
+        
+        # Something like that, it has to produce a flattened list
+        data = func(grid)
+        cube.setData(data)
         
         # Generate mesh
         mesh = self.generate_mesh(cube)
-        
         mesh_gen = Avogadro.MeshGenerator()
         
         initialized = mesh_gen.initialize(cube,mesh,iso)
@@ -63,7 +98,7 @@ class GlViewWrapper(object):
         
         self.display_mesh(mesh)
     
-    def display_mesh(mesh):
+    def display_mesh(self,mesh):
         
         surfaceEngine = self.get_engine("Surfaces")
         
@@ -102,7 +137,7 @@ def test():
     # Create isosurface from volumetric data...
     voldata = [0,1]
     wrapper.isosurface(voldata, iso = 0.1)
-
+    #print glview.method_calls
 
 if __name__ == '__main__':
     test()
