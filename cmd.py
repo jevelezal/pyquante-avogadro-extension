8cca27ba1f7a689efd0d8ed5db385ee7f9fc65a1
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2010-05-27 13:10:48 +0200

    developing an untility script

---
+++ cmd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+
+AVOEXTPATH=os.path.join(os.environ["HOME"],".avogadro/extensionScripts")
+
+LIBFILES = ["pyquante/surfaces.py"]
+
+def devel():
+    for file in LIBFILES:
+        # src dest
+        
+        dest = os.path.join(AVOEXTPATH,"lib",os.path.basename(file))
+        os.link(file,
+                dest)
+
+commands = {
+    "devel" : devel
+}
+if __name__ == '__main__':
+    import sys
+    cmd = sys.argv[1]
+    commands[cmd]()
+    
